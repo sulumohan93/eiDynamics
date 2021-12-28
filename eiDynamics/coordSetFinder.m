a4cfc6d4e10edcdf5dd7a535d5dc5d863acbf033
Aditya Asopa <aditya.ncbs@gmail.com>
2021-12-28 23:00:45 +0530

    directory got corrupted due to wrong name

--- eiDynamics/coordSetFinder.m
+++
@@ -1,6 +0,0 @@
-function coordSet = coordSetFinder(patternID)
-
-patternLUT = csvread('C:\Users\adity\OneDrive\NCBS\Lab\Projects\EI_Dynamics\Analysis\eiDynamics\patternLUT.txt');
-coordSet = nonzeros(patternLUT(patternID,2:end))';
-
-end
